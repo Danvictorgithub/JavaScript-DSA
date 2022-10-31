cc221747f9dccbc232cb5fefcf24af5bd1923b36
Deviate-DV8 <88654163+Danvictorgithub@users.noreply.github.com>
2022-10-31 20:25:55 +0800

    Added Linked list and Merge Sort Folder

---
+++ MergeSort/mergesort.js
@@ -0,0 +1,13 @@
+function mergesort(array) {
+	if (array.length == 1) {
+		return array;
+	}
+	else { 
+		if (array.length % 2 != 0) {
+			divider = (array.length - 1) / 2;
+			// left_array = 
+		}
+
+	}
+}
+array = [4,2,1,3,5];
