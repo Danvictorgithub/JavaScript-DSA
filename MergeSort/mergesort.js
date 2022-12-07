a3278dedc6292a5be6574b37044ef9552628cfe4
Deviate-DV8 <88654163+Danvictorgithub@users.noreply.github.com>
2022-12-07 19:02:18 +0800

    progress mergesrot

--- MergeSort/mergesort.js
+++ MergeSort/mergesort.js
@@ -1,13 +1,45 @@
 function mergesort(array) {
 	if (array.length == 1) {
 		return array;
-	}
-	else { 
-		if (array.length % 2 != 0) {
-			divider = (array.length - 1) / 2;
-			// left_array = 
+	} else {
+		let divider = array.length / 2;
+		const leftarray = mergesort(array.slice(0,divider));
+		const rightarray = mergesort(array.slice(divider,array.length));
+		console.log("left",leftarray);
+		console.log("right",rightarray);
+		if (leftarray > rightarray) {
+			console.log("compare","left",leftarray,"right",rightarray);	
+			array = rightarray.concat(leftarray);
+			console.log("return",array,"\n");
+			return array;
+		} else {
+			console.log("compare","left",leftarray,"right",rightarray);
+			array = leftarray.concat(rightarray);
+			console.log("return",array,"\n");
+			return array;
 		}
-
 	}
 }
-array = [4,2,1,3,5];
+let array = [5,1,3,4];
+let divider = array.length/2;
+let leftarray = array.slice(0,divider);
+let rightarray = array.slice(divider,array.length);
+// console.log(rightarray);
+
+console.log("result",array);
+// let leftarray = array.slice(0,1);
+// let rightarray = array.slice(1,2);
+
+
+// [5,1,3,4]
+// [5,1] [3,4]
+// [5] [1] [3] [4]
+// [5] > [1] [3] > [4]
+// [1,5] [3,4]
+
+// [3,4,1,5]
+// [3,4] [1,5]
+
+// let mixedarray = leftarray.concat(rightarray);
+// console.log(mergesort(array));
+
