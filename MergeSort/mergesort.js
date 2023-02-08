60ec6674ecf957e183c1a7b5a7da59e1c0b749ce
Deviate-DV8 <88654163+Danvictorgithub@users.noreply.github.com>
2023-02-08 19:34:11 +0800

    fixed misunderstanding on mergesort and replaced with correct solution

--- MergeSort/mergesort.js
+++ MergeSort/mergesort.js
@@ -1,58 +1,45 @@
-function selectionSort(array){
-	let sortedarray = [];
-	let length = array.length;
-	for (let i = 0; i < length;i++) {
-		let min = Math.min(...array);
-		sortedarray.push(min);
-		array.splice(array.indexOf(min),1);
+// function selectionSort(array){
+// 	let sortedarray = [];
+// 	let length = array.length;
+// 	for (let i = 0; i < length;i++) {
+// 		let min = Math.min(...array);
+// 		sortedarray.push(min);
+// 		array.splice(array.indexOf(min),1);
+// 	}
+// 	return sortedarray;
+// } 
+
+//Note to self: mergeSort doesn't use other sort
+function merge(lArray,rArray) {
+	let sortedArray = [];
+	let i =0,j = 0,k = 0;
+	while (i < lArray.length && j < rArray.length) {
+		if (lArray[i] < rArray[j]) {
+			sortedArray.push(lArray[i++]);
+		}
+		else {
+			sortedArray.push(rArray[j++]);
+		}
 	}
-	return sortedarray;
+	while (i < lArray.length) {
+		sortedArray.push(lArray[i++]);
+	}
+	while (j < rArray.length) {
+		sortedArray.push(rArray[j++]);
+	}
+	return sortedArray;
 }
-function mergesort(array) {
+function mergeSort(array) {
 	if (array.length == 1) {
 		return array;
 	} else {
 		let divider = array.length / 2;
-		const leftarray = mergesort(array.slice(0,divider));
-		const rightarray = mergesort(array.slice(divider,array.length));
-		console.log("left",leftarray);
-		console.log("right",rightarray);
-		if (leftarray > rightarray) {
-			console.log("compare","left",leftarray,"right",rightarray);	
-			array = selectionSort(rightarray.concat(leftarray));
-
-			console.log("return",array,"\n");
-			return array;
-		} else {
-			console.log("compare","left",leftarray,"right",rightarray);
-			array = selectionSort(leftarray.concat(rightarray));
-			console.log("return",array,"\n");
-			return array;
-		}
+		const leftarray = mergeSort(array.slice(0,divider));
+		const rightarray = mergeSort(array.slice(divider,array.length));
+		return merge(leftarray,rightarray);
 	}
 }
 let array = [5,1,3,4,5,6,8];
-// console.log(array.indexOf(5));
-// let divider = array.length/2;
-// let leftarray = array.slice(0,divider);
-// let rightarray = array.slice(divider,array.length);
-// console.log(rightarray);
-
-// console.log(insertionSort(array));
-console.log("start array:",array,"result",mergesort(array));
-// let leftarray = array.slice(0,1);
-// let rightarray = array.slice(1,2);
-
-
-// [5,1,3,4]
-// [5,1] [3,4]
-// [5] [1] [3] [4]
-// [5] > [1] [3] > [4]
-// [1,5] [3,4]
-
-// [3,4,1,5]
-// [3,4] [1,5]
-
-// let mixedarray = leftarray.concat(rightarray);
-// console.log(mergesort(array));
+console.log(mergeSort(array));
+// console.log("start array:",array,"result",mergeSort(array));
 
